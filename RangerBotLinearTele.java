67ba87f8a28099d0817351aaa5d88296c0b4c03a
Justin-Luk <42159955+Justin-Luk@users.noreply.github.com>
2018-08-07 18:17:23 -0400

    Add files via upload

---
+++ RangerBotLinearTele.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class RangerBotLinearTele extends OpMode {
+
+    public DcMotor RightDrive;
+    public DcMotor LeftDrive;
+
+
+    @Override
+    public void init() {
+
+        RightDrive = hardwareMap.dcMotor.get("RightDrive");
+        LeftDrive = hardwareMap.dcMotor.get("LeftDrive");
+
+        LeftDrive.setDirection(DcMotor.Direction.REVERSE);
+
+    }
+
+    @Override
+    public void loop() {
+
+        RightDrive.setPower(-(gamepad1.right_stick_y) / 2);
+        LeftDrive.setPower(-(gamepad1.left_stick_y) / 2);
+
+
+        while (gamepad1.right_trigger > 0) {
+            RightDrive.setDirection(DcMotor.Direction.REVERSE);
+            LeftDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        }
+    }
+}
