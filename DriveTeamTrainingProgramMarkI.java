9d0a1c35a471b59ac5ec09f1e850969b82b0e4fe
Justin-Luk <42159955+Justin-Luk@users.noreply.github.com>
2018-08-13 20:46:53 -0400

    Add files via upload

--- DriveTeamTrainingProgramMarkI.java
+++ DriveTeamTrainingProgramMarkI.java
@@ -20,6 +20,9 @@ public class DriveTeamTrainingProgramMarkI extends OpMode{
 
         RFM.setDirection(DcMotor.Direction.REVERSE);
         RBM.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addLine("Welcome,press play to begin!");
+        telemetry.addLine("To move up one level, press 'a' on gamepad1 ");
     }
 
     @Override
@@ -28,26 +31,35 @@ public class DriveTeamTrainingProgramMarkI extends OpMode{
         if (gamepad1.a) {
             level ++;
         }
-        while (level == 0) {  //Low difficulty
+        while (level == 0) {  //Low difficulty:Each driver gets a different side of the robot
             RFM.setPower(-(gamepad1.right_stick_y)/4);
             RBM.setPower(-(gamepad1.right_stick_y)/4);
             LFM.setPower(-(gamepad2.left_stick_y)/4);
             LBM.setPower(-(gamepad2.left_stick_y)/4);
+            telemetry.addLine("level 0");
         }
-        while (level == 1) {  //Moderate difficulty
+        while (level == 1) {  //Moderate difficulty: Four times faster than Level 0
             RFM.setPower(-(gamepad1.right_stick_y));
             RBM.setPower(-(gamepad1.right_stick_y));
             LFM.setPower(-(gamepad2.left_stick_y));
             LBM.setPower(-(gamepad2.left_stick_y));
-
+            telemetry.addLine("level 1");
 
         }
-        while (level == 2) {  //Insane difficulty
+        while (level == 2) {  //Insane difficulty:This tests coordination as well as communication
             RFM.setPower(-(gamepad1.right_stick_y)/4);
             RBM.setPower(-(gamepad1.left_stick_y)/4);
             LFM.setPower(-(gamepad2.right_stick_y));
             LBM.setPower(-(gamepad2.left_stick_y));
+            telemetry.addLine("level 2");
         }
+        while (level == 3) {  //
+            RFM.setPower(-(gamepad1.right_stick_y)/2);
+            RBM.setPower(-(gamepad1.right_stick_y)/2);
+            LFM.setPower(-(gamepad1.left_stick_y)/2);
+            LBM.setPower(-(gamepad1.left_stick_y)/2);
+            telemetry.addLine("level 3");
 
+        }
     }
 }
