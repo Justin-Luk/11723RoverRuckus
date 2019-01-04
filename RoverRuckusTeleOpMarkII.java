3231098264cc4b521a6e299971ab43a6ab1135d4
Justin <42159955+Justin-Luk@users.noreply.github.com>
2019-01-03 19:30:20 -0500

    Add files via upload

--- RoverRuckusTeleOpMarkII.java
+++ RoverRuckusTeleOpMarkII.java
@@ -1,9 +1,7 @@
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.robot.Robot;
 
 /**
@@ -11,73 +9,44 @@ import com.qualcomm.robotcore.robot.Robot;
  */
 @TeleOp
 public class RoverRuckusTeleOpMarkII extends OpMode {
-    private DcMotor RFM,RBM,LFM,LBM,SlideLifter,Slider, Hanger; //Establishes motors
-    private CRServo S1, S2;
-    private Servo Lockservo;
+    private DcMotor RFM,RBM,LFM,LBM; //Establishes motors
 
     @Override
     public void init() {
-        RFM = hardwareMap.dcMotor.get("RFM"); //gets RFM on hardware map
-        RBM = hardwareMap.dcMotor.get("RBM"); //gets RBM on hardware map
-        LFM = hardwareMap.dcMotor.get("LFM"); //gets LFM on hardware map
-        LBM = hardwareMap.dcMotor.get("LBM"); //gets LBM on hardware map
-        SlideLifter = hardwareMap.dcMotor.get("SlideLifter");
-        Slider = hardwareMap.dcMotor.get("Slider");
-        Hanger = hardwareMap.dcMotor.get("Hanger");
+        RFM = hardwareMap.dcMotor.get("RFM"); //gets right front motor on hardware map
+        RBM = hardwareMap.dcMotor.get("RBM"); //gets right back motor on hardware map
+        LFM = hardwareMap.dcMotor.get("LFM"); //gets left front motor on hardware map
+        LBM = hardwareMap.dcMotor.get("LBM"); //gets left back motor on hardware map
 
-        S1 = hardwareMap.crservo.get("S1");
-        S2 = hardwareMap.crservo.get("S2");
 
-        Lockservo = hardwareMap.servo.get("Lockservo");
 
         LFM.setDirection(DcMotor.Direction.REVERSE); //sets both left side motors on reverse
         LBM.setDirection(DcMotor.Direction.REVERSE);
-        S2.setDirection(CRServo.Direction.REVERSE);
     }
-
     @Override
     public void loop() {
-
-        RFM.setPower(-(gamepad1.right_stick_y)); //establishes basic tank drive controls
-        RBM.setPower(-(gamepad1.right_stick_y));
-        LFM.setPower(-(gamepad1.left_stick_y));
-        LBM.setPower(-(gamepad1.left_stick_y));
-        SlideLifter.setPower(-(gamepad2.left_stick_y)/2);
-        Slider.setPower((gamepad2.right_stick_y)/2);
-        S1.setPower(gamepad2.left_stick_y);
-        S1.setPower(gamepad2.left_stick_y);
-        Hanger.setPower(gamepad2.right_stick_x);
-
-        RFM.setPower(-(gamepad1.left_stick_x));
-        RBM.setPower(-(gamepad1.right_stick_x));
-        LFM.setPower(-(gamepad1.right_stick_x));
-        LBM.setPower(-(gamepad1.left_stick_x));
-
-        if (gamepad2.dpad_up) {
-            Lockservo.setPosition(.15);
-            telemetry.addLine("Ratchet engaged ");
-        }
-
-        if (gamepad2.dpad_down) {
-            Lockservo.setPosition(0);
-            telemetry.addLine("Ratchet disengaged ");
-        }
+        RFM.setPower(-(gamepad1.right_stick_y)/2); //establishes basic tank drive controls
+        RBM.setPower(-(gamepad1.right_stick_y)/2);
+        LFM.setPower(-(gamepad1.left_stick_y)/2);
+        LBM.setPower(-(gamepad1.left_stick_y)/2);
 
         while(gamepad1.right_trigger > 0) { //establishes strafing to the right
-
          LFM.setPower(-0.5);
          RFM.setPower(0.5);
          LBM.setPower(0.5);
          RBM.setPower(-0.5);
          }
-
          while(gamepad1.left_trigger > 0) { //establishes strafing to the left
-
            LFM.setPower(0.5);
-           RFM.setPower(-0.5);
-           LBM.setPower(-0.5);
-           RBM.setPower(0.5);
-
-    } }
-
+         RFM.setPower(-0.5);
+          LBM.setPower(-0.5);
+          RBM.setPower(0.5);
+    }
+    while (gamepad1.a){ //establishes double speed
+        LFM.setPower(-(gamepad1.left_stick_y));
+     LBM.setPower(-(gamepad1.left_stick_y));
+      RFM.setPower(-(gamepad1.right_stick_y));
+      RBM.setPower(-(gamepad1.right_stick_y));
+       }
+       }
 }
